598f25befffc5aa233880e9d245ff1829517151f
Debian <bitnami@ip-172-26-6-77.ap-northeast-1.compute.internal>
2022-09-14 18:49:21 +0000

    アカウントロック完成

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 
 
@@ -18,64 +19,85 @@ class AuthController extends Controller
         return View('login.login_form');
     }
 
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
     /**
      * @param App\Http\Requests\LoginFormRequest $request
      */
 
-    public function login(LoginFormRequest $request){
+    public function login(LoginFormRequest $request)
+    {
         $credentials = $request->only('email', 'password');
 
-        $user = User::where('email', '=', $credentials['email'])->first();
+        //$user = User::where('email', '=', $credentials['email'])->first();
+        $user = $this->user->getUserByEmail($credentials['email']);
+
         if (!is_null($user)){
 
-            if ($user->locked_flg === 1){
+            //if ($user->locked_flg === 1){
+            if ($this->user->isAccountLocked($user)){
                 return back()->withErrors([
                     'login_error' => 'アカウントがロックされています。',
                 ]);
             }
+        
 
             if (Auth::attempt($credentials)) {
                 $request->session()->regenerate();
 
-                if ($user->error_count > 0){
-                    $user->error_count = 0;
-                    $user->save();
-                }
+                //if ($user->error_count > 0){
+                //    $user->error_count = 0;
+                //    $user->save();
+                //}
+                $this->user->resetErrorCount($user);
 
                 return redirect('home')->with('login_success', 'ログインに成功しました。');
              }
 
-             $user->error_count = ($user->error_count + 1);
-             if ($user->error_count > 5){
-                $user->locked_flg = 1;
+             //$user->error_count = ($user->error_count + 1);
+             $user->error_count = $this->user->addErrorCount($user->error_count);
 
-                $user->save();
+             //if ($user->error_count > 5){
+            //    $user->locked_flg = 1;
 
+//                $user->save();
+
+  //              return back()->withErrors([
+    //                'login_error' => 'アカウントがロックされました。',
+      //          ]);
+        //    }
+
+            if ($this->user->lockAccount($user)){
                 return back()->withErrors([
                     'login_error' => 'アカウントがロックされました。',
                 ]);
             }
 
-            $user->save();
-            
+            $user->save();    
         }
 
 
         return back()->withErrors([
             'login_error' => 'メールアドレスかパスワードが間違っています。',
         ]);
-    }
+    
 
-    /**
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function logout(Request $request)
-    {
-        Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-        return redirect()->route('showLogin')->with('logout', 'ログアウトしました。');;
+        /**
+         * @param  \Illuminate\Http\Request $request
+         * @return \Illuminate\Http\Response
+         */
+        //public function logout(Request $request)
+        //{
+        //    Auth::logout();
+        //    $request->session()->invalidate();
+        //    $request->session()->regenerateToken();
+        //    return redirect()->route('showLogin')->with('logout', 'ログアウトしました。');
+        //}
     }
-
 }
+
--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,57 @@ class User extends Authenticatable
     //protected $casts = [
     //    'email_verified_at' => 'datetime',
     //];
+
+    /**
+     * emailがマッチしたユーザーを返す
+     * @param string $email
+     * @return object
+     */
+    public function getUserByEmail($email){
+        return User::where('email', '=', $email)->first();
+    }
+
+    /**
+     * アカウントがロックされているか
+     * @param object $user
+     * @return bool
+     */
+    public function isAccountLocked($user){
+        return ($user->locked_flg === 1) ? true : false;
+    }
+
+    /**
+     * エラーカウントをリセットする
+     * @param object $user
+     */
+    public function resetErrorCount($user){
+        if ($user->error_count > 0){
+            $user->error_count = 0;
+            $user->save();
+        }
+    }
+
+    /**
+     * エラーカウントを1増やす
+     * @param int $error_count
+     * @return int
+     */
+    public function addErrorCount($error_count){
+        return ($error_count + 1);
+    }
+
+    /**
+     * アカウントをロックする
+     * @param object $user
+     * @return bool
+     */
+    public function lockAccount($user){
+        if ($user->error_count > 5){
+            $user->locked_flg = 1;
+
+            return $user->save();
+        }
+
+        return false;
+    }
 }
